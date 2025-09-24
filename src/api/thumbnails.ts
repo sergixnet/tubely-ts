86eb30b011e70e92d4ce97810a259e6f73f0e2fb
Sergio Peña <sergiop79@gmail.com>
2025-09-24 19:35:09 +0200

    Encoding

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -5,37 +5,6 @@ import type { ApiConfig } from '../config';
 import type { BunRequest } from 'bun';
 import { BadRequestError, NotFoundError, UserForbiddenError } from './errors';
 
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError('Invalid video ID');
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError('Thumbnail not found');
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      'Content-Type': thumbnail.mediaType,
-      'Cache-Control': 'no-store',
-    },
-  });
-}
-
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
   if (!videoId) {
@@ -62,6 +31,7 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   const mediaType = thumbnail.type;
   const thumbnailData = await thumbnail.arrayBuffer();
+  const thumbnailBase64 = Buffer.from(thumbnailData).toString('base64');
 
   const video = await getVideo(cfg.db, videoId);
 
@@ -73,9 +43,7 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new UserForbiddenError('the user is not the owner of the video');
   }
 
-  videoThumbnails.set(video.id, { data: thumbnailData, mediaType });
-
-  const thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
+  const thumbnailURL = `data:${mediaType};base64,${thumbnailBase64}`;
   video.thumbnailURL = thumbnailURL;
 
   updateVideo(cfg.db, video);
